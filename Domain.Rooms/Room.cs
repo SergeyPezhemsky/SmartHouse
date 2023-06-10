067974c049e3123dbcc7fd1090dccf58325a9d86
Diana <sergey.pezhemsky@gmail.com>
2023-06-10 22:14:05 +0300

    Initial Commit + add room class

---
+++ Domain.Rooms/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Domain.Rooms;
+
+public class Room
+{
+    public string Name { get; set; }
+
+    public Room(string name)
+    {
+        Name = name;
+    }
+
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+}
